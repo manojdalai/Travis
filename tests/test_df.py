f7cc1211157acca09f8b284becc42aeb828feb41
manojdalai <manoj.uce10@gmail.com>
2021-04-10 14:08:35 +0200

    adding pyspark code

--- tests/test_df.py
+++ tests/test_df.py
@@ -29,38 +29,7 @@ def test_remove_non_word_characters_short():
     ]
     df1 = (spark.createDataFrame(data, ["name", "expected_name"])
               .withColumn("clean_name", remove_non_word_characters(F.col("name"))))
-    print(assert_column_equality(df1, "clean_name", "expected_name"))
+    assert_column_equality(df1, "clean_name", "expected_name")
 
-def test_remove_non_word_characters_nice_error():
-    data = [
-        ("matt7", "matt"),
-        ("bill&", "bill"),
-        ("isabela*", "isabela"),
-        (None, None)
-    ]
-    df = (spark.createDataFrame(data, ["name", "expected_name"])
-          .withColumn("clean_name", remove_non_word_characters(F.col("name"))))
-    print(assert_column_equality(df, "clean_name", "expected_name"))
 
-def test_remove_non_word_characters_long():
-    source_data = [
-        ("jo&&se",),
-        ("**li**",),
-        ("#::luisa",),
-        (None,)
-    ]
-    source_df = spark.createDataFrame(source_data, ["name"])
-    actual_df = source_df.withColumn(
-        "clean_name",
-        remove_non_word_characters(F.col("name"))
-    )
-
-    expected_data = [
-        ("jo&&se", "jose"),
-        ("**li**", "li"),
-        ("#::luisa", "luisa"),
-        (None, None)
-    ]
-    expected_df = spark.createDataFrame(expected_data, ["name", "clean_name"])
-    assert_df_equality(actual_df, expected_df)
 
